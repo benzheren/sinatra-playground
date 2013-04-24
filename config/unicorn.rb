457c109f28003d7e2b4826b988661a5d19f7c941
Ben Hu <benzheren@gmail.com>
2013-04-24 22:23:19 +0800

    fix copy paste of unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -34,7 +34,7 @@ before_fork do |server, worker|
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
-  old_pid = "#{Rails.root}/tmp/pids/unicorn.pid.oldbin"
+  old_pid = "#{APP_PATH}/tmp/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
